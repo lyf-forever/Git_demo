f0b0e12fe84dc1b10df61ac16c2848abd2db2107
lyf-forever <1956821899@qq.com>
2025-08-14 23:08:09 +0800

    main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: lyf-forever 1956821899@qq.com
+ * @Date: 2025-08-14 23:06:18
+ * @LastEditors: lyf-forever 1956821899@qq.com
+ * @LastEditTime: 2025-08-14 23:06:28
+ * @FilePath: \Git_demo\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+JSON
